911fa9f00f32f0071b1e1a08cbaa272ec30baa0e
Emmanuel Londoño Gomez <emmalondogo@gmail.com>
2023-04-26 14:16:02 -0500

    nueva mejoras en el home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',loadChildren: () => import('./presentation/presentation-routing.module').then((module) => module.PresentationRoutingModule) }];
+  {path:''}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/presentation/presentation.module.ts
+++ src/app/presentation/presentation.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from "@angular/core";
 import { CoreModule } from "../core/core.module";
 import { DataModule } from "../data/data.module";
 import { PresentationRoutingModule } from "./presentation-routing.module";
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [CoreModule, DataModule, PresentationRoutingModule],
-  exports: []
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ]
 })
 export class PresentationModule{}
