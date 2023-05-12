4352d87594d4978bb0ddf756e5dac29189a0d0a0
Emmanuel Londoño Gomez <emmalondogo@gmail.com>
2023-05-12 16:23:14 -0500

    mejoras en home,list,offers

--- src/app/presentation/offers/list/list.component.ts
+++ src/app/presentation/offers/list/list.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+listOffers = [
+    {
+      id: 1,
+      title: "Software Developer",
+      status: 'Activa',
+      date_status: 'Enviada a revisión: 2023-05-11 - 11:45',
+      location: "Medellín y sus alrededores",
+      money: "De $1 a $5 millones",
+      modality: "Remoto"
+    },
+    {
+      id: 2,
+      title: "Database Administrator",
+      status: 'En Revision',
+      date_status: 'Enviada a revisión: 2023-05-11 - 11:45',
+      location: "Medellín y sus alrededores",
+      money: "De $1 a $5 millones",
+      modality: "Remoto"
+    },
+     {
+      id: 3,
+      title: "Database Administrator",
+      status: 'En Revision',
+      date_status: 'Enviada a revisión: 2023-05-11 - 11:45',
+      location: "Medellín y sus alrededores",
+      money: "De $1 a $5 millones",
+      modality: "Remoto"
+    },
+     {
+      id: 4,
+      title: "Database Administrator",
+      status: 'En Revision',
+      date_status: 'Enviada a revisión: 2023-05-11 - 11:45',
+      location: "Medellín y sus alrededores",
+      money: "De $1 a $5 millones",
+      modality: "Remoto"
+    },
+     {
+      id: 5,
+      title: "Database Administrator",
+      status: 'En Revision',
+      date_status: 'Enviada a revisión: 2023-05-11 - 11:45',
+      location: "Medellín y sus alrededores",
+      money: "De $1 a $5 millones",
+      modality: "Remoto"
+    },
 
+  ];
 }
--- src/app/presentation/offers/main-offers/main-offers.component.ts
+++ src/app/presentation/offers/main-offers/main-offers.component.ts
@@ -10,6 +10,17 @@ export class MainOffersComponent {
     {
       id: 1,
       title: "Software Developer",
+      status: 'Activa',
+      date_status: 'Enviada a revisión: 2023-05-11 - 11:45',
+      location: "Medellín y sus alrededores",
+      money: "De $1 a $5 millones",
+      modality: "Remoto"
+    },
+    {
+      id: 1,
+      title: "Database Administrator",
+      status: 'En Revision',
+      date_status: 'Enviada a revisión: 2023-05-11 - 11:45',
       location: "Medellín y sus alrededores",
       money: "De $1 a $5 millones",
       modality: "Remoto"
