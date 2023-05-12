4108c26811b0ce9de36990e9d8f9a546d4ece775
Emmanuel Londoño Gomez <emmalondogo@gmail.com>
2023-05-12 12:40:34 -0500

    agregacion de modulos y cambios en routings

--- src/app/presentation/presentation-routing.module.ts
+++ src/app/presentation/presentation-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ListComponent } from './list/list.component';
+import { ListComponent } from './offers/list/list.component';
 import { DetailsComponent } from './details/details.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AuthModule } from './auth/auth.module';
 
 const routes: Routes = [
   {
@@ -13,21 +13,20 @@ const routes: Routes = [
   },
   {
     path:'login',
-    component:LoginComponent
+    loadChildren:()=>import('./auth/login/login.component').then((module)=>module.LoginComponent),
   },
   {
     path: 'list',
-    component: ListComponent
+    loadChildren:()=>import('./offers/list/list.component').then((module)=>module.ListComponent),
   },
   {
     path:"details",
     component: DetailsComponent
   },
   {
-    path:"dashboard",
-    component: DashboardComponent
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then((module)=>module.DashboardModule),
   }
-
 ];
 
 @NgModule({
